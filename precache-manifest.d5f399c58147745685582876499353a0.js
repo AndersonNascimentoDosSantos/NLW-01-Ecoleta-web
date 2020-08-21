2a0535b6e496a2ba169278d45bea5194b92649a2
anderson nascimento dos santos <anderson@localhost.localdomain>
2020-08-21 07:14:38 -0300

    Updates

---
+++ precache-manifest.d5f399c58147745685582876499353a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8ba9545f05738c7b9fc9117274eac8",
+    "url": "/NLW-02-Ecoleta-Web/index.html"
+  },
+  {
+    "revision": "a9b9adb9159117e31b16",
+    "url": "/NLW-02-Ecoleta-Web/static/css/main.0e2aa25a.chunk.css"
+  },
+  {
+    "revision": "f42b6732981b573f9a34",
+    "url": "/NLW-02-Ecoleta-Web/static/js/2.474e283d.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/NLW-02-Ecoleta-Web/static/js/2.474e283d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b9adb9159117e31b16",
+    "url": "/NLW-02-Ecoleta-Web/static/js/main.821235bd.chunk.js"
+  },
+  {
+    "revision": "4795c1fbf5296bfeb5f3",
+    "url": "/NLW-02-Ecoleta-Web/static/js/runtime-main.aa0a7f6a.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/NLW-02-Ecoleta-Web/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/NLW-02-Ecoleta-Web/static/media/logo.47c4581a.svg"
+  }
+]);
