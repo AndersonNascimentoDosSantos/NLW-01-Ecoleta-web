2fdc59f8077e902dfaac8afa015fe168ce8b588e
anderson nascimento dos santos <anderson@localhost.localdomain>
2020-08-21 08:27:12 -0300

    Updates

---
+++ precache-manifest.2e78a187ccc9cc98d72a9d056bed6855.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61b5b9da38de7b55c2ed587bfb9b23e",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/index.html"
+  },
+  {
+    "revision": "9c0b94666f358e7aab2f",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/css/main.210af9f6.chunk.css"
+  },
+  {
+    "revision": "16c606971ef9cc98a44b",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/js/2.66bd9d3b.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/js/2.66bd9d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0b94666f358e7aab2f",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/js/main.0360cd39.chunk.js"
+  },
+  {
+    "revision": "18f99c3cccad3bfbdf3a",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/js/runtime-main.e3127284.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/tecanderson.github.io/NLW-01-Ecoleta-web/static/media/logo.47c4581a.svg"
+  }
+]);
